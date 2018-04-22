cabdd2dc4566f51f3971e6e08002971147107ab1
AlaskaJay <thatthinker@gmail.com>
2018-04-22 02:04:15 -0400

    Add files via upload

---
+++ DigitalModularSynth/src/main/ComponentTesting.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import model.Component;
+import model.Sequencer;
+
+public class ComponentTesting {
+
+	public static void main(String[] args) {
+		Queue<Byte> sequence = new LinkedList<Byte>();
+		
+		for(Byte i = 0; i < 10; i++)
+			sequence.offer((byte) (i+100));
+		
+		Component feed = new Component(new Component(new Component(new Sequencer(sequence, "squencer"), "between 1"), "between 2"), "feed");
+		
+		for(int i = 0; i < 100; i++) {
+			System.out.println(i +"," + feed.processData());
+		}
+		
+		
+	}
+
+}
